9466dd544cb5e56027a91c1ba5ee586959885462
omoch1 <luenauxle.myphaxem@gmail.com>
2026-07-11 17:20:02 +0900

    Disable automatic third-party seed callbacks

--- src/main/java/kaptainwutax/seedcrackerX/SeedCracker.java
+++ src/main/java/kaptainwutax/seedcrackerX/SeedCracker.java
@@ -8,14 +8,21 @@ import kaptainwutax.seedcrackerX.finder.FinderQueue;
 import kaptainwutax.seedcrackerX.init.ClientCommands;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 
 import java.util.ArrayList;
 
 public class SeedCracker implements ModInitializer {
     public static final Logger LOGGER = LogUtils.getLogger();
+
+    /**
+     * Kept for source/binary compatibility with the existing API. The
+     * local-only build deliberately does not discover third-party
+     * "seedcrackerx" entrypoints, so no other mod is automatically handed a
+     * cracked world seed.
+     */
     public static final ArrayList<SeedCrackerAPI> entrypoints = new ArrayList<>();
+
     private static SeedCracker INSTANCE;
     private final DataStorage dataStorage = new DataStorage();
 
@@ -28,8 +35,6 @@ public class SeedCracker implements ModInitializer {
         INSTANCE = this;
         Config.load();
         Features.init(Config.get().getVersion());
-        FabricLoader.getInstance().getEntrypointContainers("seedcrackerx", SeedCrackerAPI.class).forEach(entrypoint ->
-                entrypoints.add(entrypoint.getEntrypoint()));
 
         FinderQueue.registerEvents();
 
