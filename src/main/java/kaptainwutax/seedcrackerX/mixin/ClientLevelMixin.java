4afe59d1954a619705453967185391be33c95d6c
omoch1 <luenauxle.myphaxem@gmail.com>
2026-07-11 23:44:11 +0900

    Remove biome override unrelated to seed cracking

--- src/main/java/kaptainwutax/seedcrackerX/mixin/ClientLevelMixin.java
+++ src/main/java/kaptainwutax/seedcrackerX/mixin/ClientLevelMixin.java
@@ -5,19 +5,15 @@ import kaptainwutax.seedcrackerX.config.StructureSave;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.storage.WritableLevelData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientLevel.class)
 public abstract class ClientLevelMixin extends Level {
@@ -30,11 +26,5 @@ public abstract class ClientLevelMixin extends Level {
     private void disconnect(Component reason, CallbackInfo ci) {
         StructureSave.saveStructures(SeedCracker.get().getDataStorage().baseSeedData);
         SeedCracker.get().reset();
-    }
-
-    @Inject(method = "getUncachedNoiseBiome", at = @At("HEAD"), cancellable = true)
-    private void getGeneratorStoredBiome(int x, int y, int z, CallbackInfoReturnable<Holder<Biome>> ci) {
-        var biome = registryAccess().lookupOrThrow(Registries.BIOME).get(Biomes.THE_VOID);
-        biome.ifPresent(ci::setReturnValue);
     }
 }
